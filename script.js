c3267ea4f6e0eca0d8b8a3fe2dad12cf77038ab6
Rigon <rigon.osmanaj@beriflapp.com>
2024-12-13 14:44:04 +0100

    Traffic ligh -Created a traffic light object usig a div container and buttons -Used js to show bootstrap alerts based on the button clicked

---
+++ script.js
@@ -0,0 +1,57 @@
+const alertBox = document.getElementById("trafficLightAlert");
+const iconContainer = document.getElementById("iconContainer");
+const alertText = document.getElementById("alertText");
+
+const redLight = document.querySelector(".red-light");
+const orangeLight = document.querySelector(".orange-light");
+const greenLight = document.querySelector(".green-light");
+
+// Icons
+const icons = {
+  red: `
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sign-stop" viewBox="0 0 16 16">
+      <path d="M3.16 10.08c-.931 0-1.447-.493-1.494-1.132h.653c.065.346.396.583.891.583.524 0 .83-.246.83-.62 0-.303-.203-.467-.637-.572l-.656-.164c-.61-.147-.978-.51-.978-1.078 0-.706.597-1.184 1.444-1.184.853 0 1.386.475 1.436 1.087h-.645c-.064-.32-.352-.542-.797-.542-.472 0-.77.246-.77.6 0 .261.196.437.553.522l.654.161c.673.164 1.06.487 1.06 1.11 0 .736-.574 1.228-1.544 1.228Zm3.427-3.51V10h-.665V6.57H4.753V6h3.006v.568H6.587Z"/>
+      <path fill-rule="evenodd" d="M11.045 7.73v.544c0 1.131-.636 1.805-1.661 1.805-1.026 0-1.664-.674-1.664-1.805V7.73c0-1.136.638-1.807 1.664-1.807s1.66.674 1.66 1.807Zm-.674.547v-.553c0-.827-.422-1.234-.987-1.234-.572 0-.99.407-.99 1.234v.553c0 .83.418 1.237.99 1.237.565 0 .987-.408.987-1.237m1.15-2.276h1.535c.82 0 1.316.55 1.316 1.292 0 .747-.501 1.289-1.321 1.289h-.865V10h-.665zm1.436 2.036c.463 0 .735-.272.735-.744s-.272-.741-.735-.741h-.774v1.485z"/>
+      <path fill-rule="evenodd" d="M4.893 0a.5.5 0 0 0-.353.146L.146 4.54A.5.5 0 0 0 0 4.893v6.214a.5.5 0 0 0 .146.353l4.394 4.394a.5.5 0 0 0 .353.146h6.214a.5.5 0 0 0 .353-.146l4.394-4.394a.5.5 0 0 0 .146-.353V4.893a.5.5 0 0 0-.146-.353L11.46.146A.5.5 0 0 0 11.107 0zM1 5.1 5.1 1h5.8L15 5.1v5.8L10.9 15H5.1L1 10.9z"/>
+    </svg>`,
+  orange: `
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle" viewBox="0 0 16 16">
+      <path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.15.15 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.2.2 0 0 1-.054.06.1.1 0 0 1-.066.017H1.146a.1.1 0 0 1-.066-.017.2.2 0 0 1-.054-.06.18.18 0 0 1 .002-.183L7.884 2.073a.15.15 0 0 1 .054-.057m1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767z"/>
+      <path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>
+    </svg>`,
+  green: `
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
+      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+      <path d="M10.97 6.97a.75.75 0 0 1 0 1.06L7.477 11.525a.75.75 0 0 1-1.06 0L5.03 10.138a.75.75 0 1 1 1.06-1.06L7 9.061l2.47-2.47a.75.75 0 0 1 1.06 0z"/>
+    </svg>`,
+};
+
+function showAlert(type, message, iconKey) {
+  if (iconContainer) {
+    iconContainer.innerHTML = icons[iconKey];
+  }
+
+  alertBox.className = "alert align-items-center";
+
+  alertBox.classList.add(`alert-${type}`, "show");
+
+  if (alertText) {
+    alertText.textContent = message;
+  }
+
+  alertBox.style.display = "block";
+  setTimeout(() => {
+    alertBox.style.display = "none";
+  }, 3000);
+}
+
+// Event Listeners
+redLight.addEventListener("click", () =>
+  showAlert("danger", "Stop! The light is red.", "red")
+);
+orangeLight.addEventListener("click", () =>
+  showAlert("warning", "Caution! The light is yellow.", "orange")
+);
+greenLight.addEventListener("click", () =>
+  showAlert("success", "Go! The light is green.", "green")
+);
